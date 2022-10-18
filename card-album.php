b25caa5e3795c7c722a221327e592e92f7068fc0
Angelo <Angelodimauro91@gmail.com>
2022-10-18 23:53:16 +0200

    end PHP version

---
+++ card-album.php
@@ -0,0 +1,13 @@
+
+<div class="container">
+    <div class="card-album">
+        
+        <div class="image">
+            <img src="<?= $album['poster'] ?>" alt="<?= $album['title'] ?>" />
+        </div>
+
+        <h2><?= $album['title'] ?></h2>
+        <p><?= $album['year'] ?></p>
+        <p><?= $album['author'] ?></p>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css">
     <title>Document</title>
 </head>
 <body>
@@ -12,16 +13,11 @@ include "dataBase.php";
 foreach($dataBase as $album){
 ?>
 
-
-
-
 <div>
     <?php
-     include 'card.php'
+     include 'card-album.php'
      ?>
 </div>
-
-
 
 <?php
 }
